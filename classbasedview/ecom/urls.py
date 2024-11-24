ba15da7bf53d7a303fca5cb340721be95914011b
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-11-25 00:31:50 +0530

    created the url for the add

--- classbasedview/ecom/urls.py
+++ classbasedview/ecom/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from classbasedview.ecom.views import HomeView
+
+from classbasedview.ecom.management import ProductAddView
+from classbasedview.ecom.views import HomeView, SuperAdminDashboardView
 
 app_name = "ecom"
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
+]
+# super admin dashboard
+urlpatterns += [
+    path('dashbord/', login_required(SuperAdminDashboardView.as_view()), name="super_admin_dashboard"),
+    path('add/product/', login_required(ProductAddView.as_view()), name="add_product"),
 ]

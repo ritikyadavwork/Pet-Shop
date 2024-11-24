b7707a94965afa0c49034577950d93d030ac8dd1
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-11-25 00:27:02 +0530

    created the model and runs migrations

--- classbasedview/ecom/admin.py
+++ classbasedview/ecom/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from classbasedview.ecom.models import Product
+
 
 # Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'stock')
--- classbasedview/ecom/models.py
+++ classbasedview/ecom/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from model_utils.models import TimeStampedModel
+from ckeditor.fields import RichTextField
+
 
 # Create your models here.
+
+class Product(TimeStampedModel):
+    title = models.CharField(max_length=100)
+    product_name = models.CharField(max_length=255, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField(default=0)
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    description = RichTextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title

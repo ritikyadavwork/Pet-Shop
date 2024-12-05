a147e2527eb376bcf37fd21bf8ca3c2ff3e4ed09
Ritik Yadav <ritikyadavwork23@gmail.com>
2024-12-05 13:11:30 +0530

    added a field in product model and runs migrations

--- classbasedview/ecom/models.py
+++ classbasedview/ecom/models.py
@@ -12,6 +12,7 @@ class Product(TimeStampedModel):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
     image = models.ImageField(upload_to='products/', null=True, blank=True)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
     description = RichTextField(null=True, blank=True)
 
     def __str__(self):
